b60a75b417c74471bc195c6fd2cb1aec14830ca3
David Hernandez <hdz.david020@gmail.com>
2023-09-27 00:08:58 -0600

    feat: :sparkles: add routes, layout and initial theme

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,8 +1,29 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
+	content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
 	theme: {
-		extend: {},
+		extend: {
+			fontFamily: {
+				bungue: ["Bungee Shade"],
+			},
+		},
 	},
 	plugins: [require("daisyui")],
-}
+	daisyui: {
+		themes: [
+			{
+				mytheme: {
+					"primary": "#041c32",
+					"secondary": "#064663",
+					"accent": "#b2ab8c",
+					"neutral": "#44403c",
+					"base-100": "#1c1917",
+					"info": "#ecb365",
+					"success": "#0f766e",
+					"warning": "#6b21a8",
+					"error": "#9d174d",
+				},
+			},
+		],
+	},
+};
